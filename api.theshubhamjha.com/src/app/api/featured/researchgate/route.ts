d7f155949d08c1f29f1c47e69d4041f95d531331
shubhamjha2510 <30436356+shubhamjha2510@users.noreply.github.com>
2025-06-27 20:34:46 +0530

    Delete api.theshubhamjha.com directory

--- api.theshubhamjha.com/src/app/api/featured/researchgate/route.ts
+++
@@ -1,9 +0,0 @@
-import { FeaturedProjects } from "../Types";
-import { FrozenResponse } from "../utils";
-import json from "./response.json";
-
-export const dynamic = "force-dynamic"; // static by default, unless reading the request
-
-export async function GET() {
-  return new FrozenResponse(json as FeaturedProjects);
-}
